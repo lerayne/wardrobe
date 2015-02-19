a072c0e2ec6eecfdf6a03f5d4e0c0dae9065e2e5
Michael Yegorov <lerayne@gmail.com>
2015-02-19 13:13:52 +0200

    layout, outdated warning

--- script/main.js
+++ script/main.js
@@ -4,6 +4,40 @@
 
 $(function(){
 
-	$('body').append('<x-app>');
+	var onPolymerReady = function(){
+
+	};
+
+	var onDOMReady = function(){
+		$('body').append('<x-app>');
+	}
+
+
+
+
+	// initialization part 1: Basic DOM loaded
+	$(function(){
+		console.info('Root DOM ready');
+
+		// If this is not IE or its IE 10 - launch the webapp (IE11 and greater aren't having "MSIE" in userAgent)
+		if (navigator.userAgent.indexOf('MSIE') == -1 || navigator.userAgent.indexOf('MSIE 10') != -1) {
+
+			onDOMReady();
+
+		} else {
+			// get outdated-browser warning
+			$.get('includes/outdated.html').then(function(html){
+				$('body').append(html);
+			});
+		}
+	});
+
+
+	// initialization part 2: Polymer finished it's startup
+	$(window).on('polymer-ready', function(){
+		console.info('Polymer ready');
+
+		onPolymerReady();
+	});
 
 })
