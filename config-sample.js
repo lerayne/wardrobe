77918e4b5acfa25c54c21b21c99113f51f635432
Michael Yegorov <lerayne@gmail.com>
2015-02-25 22:44:17 +0200

    config location changed, resize method changed

---
+++ config-sample.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Lerayne on 25.02.2015.
+ */
+
+app.cfg = {
+	server_url:'http://localhost/wardrobe/server',
+	logging: false,
+	poll_timer: 5000,
+	poll_timer_blurred: 60000,
+	maintenance:false
+}
--- project-init.sh
+++ project-init.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 project-update.sh
 cp config-sample.html config.html
+cp config-sample.js config.js
